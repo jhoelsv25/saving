d3cf1130125e64957d236b66086cfbf3dca1ce25
Jhoel <jhoel.sv25@gmail.com>
2025-05-30 21:39:14 -0500

    feat: update app component to include toast service and component, add user module with routes and component, enhance global styles with theme variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastService } from './core/services/toast.service';
+import { ToastComponent } from '@components/toast/toast.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, ToastComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'saving-01';
+    private toastService = inject(ToastService);
+
+    title = 'saving-01';
 }
---
+++ src/app/client/modules/users/pages/users/users.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-users',
+    imports: [],
+    templateUrl: './users.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class UsersComponent {}
