6321dbddd9d9f8ce55aa6610d60968f096b095eb
Jhoel <jhoel.sv25@gmail.com>
2025-05-30 23:27:04 -0500

    feat: implement layout component with sidebar and header, add toggle functionality for sidebar

--- src/app/client/modules/layouts/layout/layout.component.ts
+++ src/app/client/modules/layouts/layout/layout.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { HttpHeaderResponse } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+import { HeaderComponent } from '../header/header.component';
+import { RouterOutlet } from '@angular/router';
+import { slideInLeftAnimation } from '@shared/utils/animation';
 
 @Component({
-  selector: 'app-layout',
-  imports: [],
-  templateUrl: './layout.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-layout',
+    imports: [NgClass, HeaderComponent, SideNavComponent, RouterOutlet],
+    templateUrl: './layout.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [slideInLeftAnimation('openClose', '300px')],
 })
-export class LayoutComponent { }
+export default class LayoutComponent {
+    public sidebarOpen = signal<boolean>(false);
+
+    public toggleSidebar() {
+        this.sidebarOpen.update(prev => !prev);
+    }
+}
